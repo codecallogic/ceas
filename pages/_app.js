8d7d4c0532385deec512877d3f7dccb074a5a58e
Fabricio <contact@fabricioguardia.com>
2021-12-01 23:54:11 -0800

    set up sass styles and svg data

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/app.css'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <>
+      <title>CEAS</title>
+      <meta charset="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+      <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet" />
+      </>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
---
+++ pages/admin/login.js
@@ -0,0 +1,19 @@
+import SVG from '../../files/svg'
+
+const AdminLogin = ({}) => {
+  
+  return (
+    <div className="adminLogin">
+      <div className="adminLogin-container">
+        <div className="adminLogin-left">
+          <SVG svg={'remotely'} color={'#e63946'}></SVG>
+        </div>
+        <div className="adminLogin-right">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AdminLogin
