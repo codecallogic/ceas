eff7ff02a6e515c36721d4bc8175058cb613535c
Fabricio <contact@fabricioguardia.com>
2022-01-02 01:15:16 -0800

    table design and controls

--- components/table.jsx
+++ components/table.jsx
@@ -1,25 +1,102 @@
 import {filterTable} from '../helpers/tables'
+import SVG from '../files/svg'
+import {useEffect, useState, useRef} from 'react'
 
 const Table = ({
   title,
   adminUsers
 }) => {
-  console.log(adminUsers)
+
+  const myRefs = useRef([])
+  const [selectID, setSelectID] = useState('')
+  const [controls, setControls] = useState(false)
+
+  const handleClickOutside = (event) => {
+    if(myRefs.current){
+      myRefs.current.forEach((item) => {
+        if(item){
+          if(item.contains(event.target)) return
+          if(event.target == document.getElementById('delete-job')) return
+          if(event.target == document.getElementById('edit-job')) return
+
+          const els = document.querySelectorAll('.table-rows-checkbox-input')
+          els.forEach( (el) => { el.checked = false })
+
+          setControls(false)
+          setSelectID('')
+
+        }
+      })
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [selectID])
+
+  const handleSelect = (e, id) => {
+    const els = document.querySelectorAll('.table-rows-checkbox-input')
+    els.forEach( (el) => { el.checked = false })
+    e.target.checked = true
+
+    setControls(true)
+    setSelectID(id)
+  }
+
   return (
     <div className="table">
       <div className="table-header">
         <div className="table-header-title">{title}</div>
+        {controls &&
+          <div className="table-header-controls">
+            <div className="table-header-controls-item">Edit</div>
+            <div className="table-header-controls-item">Delete</div>
+          </div>
+        }
       </div>
       <div className="table-headers">
         <div className="table-headers-item">&nbsp;</div>
         { 
-          filterTable(adminUsers).length > 0 && filterTable(adminUsers, ['_id', 'createdAt', 'updatedAt', '__v'], 1).map((item, idx, array) => 
+          filterTable(adminUsers).length > 0 && 
+          filterTable(adminUsers, ['_id', 'createdAt', 'updatedAt', '__v'], 1).map((item, idx, array) => 
             Object.keys(array[0]).map((key, idx) => 
-              <div key={idx} className="table-headers-item">{key}</div>
+              <div key={idx} className="table-headers-item">
+                {key.replace( /([a-z])([A-Z])/g, "$1 $2")}
+              </div>
             )
           )
         }
       </div>
+      <div className="table-rows-container">
+      { 
+        filterTable(adminUsers).length > 0 && 
+        filterTable(adminUsers, ['createdAt', 'updatedAt', '__v']).map((item, idx) => 
+          <div key={idx} className="table-rows">
+            <div className="table-rows-checkbox" 
+              ref={(el) => (myRefs.current[idx] = el)}
+            >
+              <label>
+                <input className="table-rows-checkbox-input" type="checkbox" onClick={(e) => handleSelect(e, item._id)}/>
+                <span></span>
+                <div>
+                  <SVG svg={'checkmark'}></SVG>
+                </div>
+              </label>
+            </div>
+            <div className="table-rows-item">{item.firstName}</div>
+            <div className="table-rows-item">{item.lastName}</div>
+            <div className="table-rows-item">{item.email}</div>
+            <div className="table-rows-item">{item.username}</div>
+            <div className="table-rows-item">{item.role}</div>
+          </div>
+        )
+      }
+      </div>
+     
     </div>
   )
 }
