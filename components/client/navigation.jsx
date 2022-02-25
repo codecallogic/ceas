e24ec00ca10ff5ae696a0b3cb5e60af22a2d9602
Fabricio <contact@fabricioguardia.com>
2022-02-25 01:21:38 -0800

    socket connection

---
+++ components/client/navigation.jsx
@@ -0,0 +1,14 @@
+
+const Navigation = ({}) => {
+  
+  return (
+    <div className="nav">
+      <a href="#"><img className="nav-logo" src="./logo_nav.png" alt="ceas-logo" /></a>
+      <div className="nav-menu">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navigation
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
-export default function Home() {
+import { useState, useEffect } from 'react'
+import DesktopNav from '../components/client/navigation'
+import io from 'socket.io-client'
+import { SOCKET } from '../config';
+
+const socket = io.connect(SOCKET, {transports: ['websocket', 'polling', 'flashsocket']});
+
+const Home = ({}) => {
+  
+  useEffect(() => {
+    
+    socket.on('navigation', (data) => {
+      console.log(data)
+      data.forEach((item) => {
+        
+        console.log(item.link.split("//")[1].split('/').slice(1).join('/'))
+        
+      })
+    })
+  }, [])
+  
   return (
-    <div className="home">
-      Website is under development...
-      <a onClick={() => window.location.href = '/admin/login'}>Login</a>
+    <div className="home wrapper">
+      <DesktopNav></DesktopNav>
     </div>
   )
 }
+
+export default Home
