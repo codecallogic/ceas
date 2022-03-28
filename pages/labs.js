8dd4debae4be6d8c7921d3a83e090cfa69134ffe
Fabricio <contact@fabricioguardia.com>
2022-03-28 11:39:08 -0700

    lab single view component

--- pages/labs.js
+++ pages/labs.js
@@ -35,21 +35,22 @@ const Equipment = ({
             <div 
               key={idx} 
               className="lab-section-1-announcement "
-              onClick={() => window.location = `/equipment?title=${item.name}`}
+              onClick={() => window.location = `/labs?title=${item.name}`}
             >
               <div className="lab-section-1-announcement-title">{item.name} - {item.labLocation}</div>
               <div className="lab-section-1-announcement-content">
                 <div className="lab-section-1-announcement-content-researcher">
-                  <h1>Lead Researcher</h1>
                   <img 
                     src={`${PUBLIC_FILES}/faculty/${item.faculty[0] ? item.faculty[0].image : ''}`}
                     alt={item.faculty[0] ? item.faculty[0].name : ''}
                     onError={(e) => e.target.src = 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled.png'}
                   />
-                  <h2>{item.faculty[0] ? item.faculty[0].title : ''} {item.faculty[0] ? item.faculty[0].name : ''}</h2>
-                  <h3>{item.faculty[0] ? item.faculty[0].profession : ''}</h3>
-                  <h3>{item.faculty[0] ? item.faculty[0].email : ''}</h3>
-                  <h3>{item.faculty[0] ? item.faculty[0].officePhone : ''}</h3>
+                  <div className="lab-section-1-announcement-content-researcher-content">
+                    <h2>{item.faculty[0] ? item.faculty[0].title : ''} {item.faculty[0] ? item.faculty[0].name : ''}</h2>
+                    <h3>{item.faculty[0] ? item.faculty[0].profession : ''}</h3>
+                    <h3>{item.faculty[0] ? item.faculty[0].email : ''}</h3>
+                    <h3>{item.faculty[0] ? item.faculty[0].officePhone : ''}</h3>
+                  </div>
                 </div>
                 <div className="lab-section-1-announcement-content-lab">
                   <img 
@@ -62,6 +63,40 @@ const Equipment = ({
                 </div>
               </div>
             </div>
+          )}
+
+          {title && labs.length > 0 && labs.map((item, idx) => 
+            item.name == title 
+            ?
+            <div 
+              key={idx} 
+              className="lab-section-2-announcement"
+            >
+              <div className="lab-section-2-researcher">
+                <img 
+                  src={`${PUBLIC_FILES}/faculty/${item.faculty[0] ? item.faculty[0].image : ''}`}
+                  alt={item.faculty[0].name}
+                  onError={(e) => e.target.src = 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled.png'}
+                />
+                <div className="lab-section-2-researcher-content">
+                  <h2>{item.faculty[0] ? item.faculty[0].title : ''} {item.faculty[0] ? item.faculty[0].name : ''}</h2>
+                  <h3>{item.faculty[0] ? item.faculty[0].profession : ''}</h3>
+                  <h3>{item.faculty[0] ? item.faculty[0].email : ''}</h3>
+                  <h3>{item.faculty[0] ? item.faculty[0].officePhone : ''}</h3>
+                </div>
+              </div>
+              <div className="lab-section-2-announcement-title">{item.name} - {item.labLocation}</div>
+              <div className="lab-section-2-announcement-image">              
+                <img 
+                  src={`${PUBLIC_FILES}/labs/${item.image}`} 
+                  alt={item.name}
+                  onError={(e) => e.target.src = 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled.png'}
+                />
+              </div>
+              <div className="lab-section-2-announcement-paragraph" dangerouslySetInnerHTML={{ __html: `${item.description.substring(0, 500)}...`}}></div>
+            </div>
+            :
+            null
           )}
         </div>
       </div>
