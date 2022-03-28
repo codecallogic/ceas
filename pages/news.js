9e57ff45dca29fea0724b0f180af2e72ea29b545
Fabricio <contact@fabricioguardia.com>
2022-03-27 22:52:53 -0700

    single news view component

--- pages/news.js
+++ pages/news.js
@@ -17,6 +17,10 @@ const News = ({
   
 }) => {
 
+  useEffect(() => {
+    console.log(title)
+  }, [title])
+
   return (
     <>
       <Toolbar></Toolbar>
@@ -27,8 +31,12 @@ const News = ({
       ></Navigation>
       <div className="news">
         <div className="news-section-1 wrapper">
-          {news.length > 0 && news.sort((a, b) => new Date(a.date) > new Date(b.date) ? -1 : 1).map((item, idx) => 
-            <div key={idx} className="news-section-1-announcement">
+          {!title && news.length > 0 && news.sort((a, b) => new Date(a.date) > new Date(b.date) ? -1 : 1).map((item, idx) => 
+            <div 
+              key={idx} 
+              className="news-section-1-announcement"
+              onClick={() => window.location = `/news?title=${item.title}`}
+            >
               <div className="news-section-1-announcement-title">{item.title} - {item.date}</div>
               <div className="news-section-1-announcement-content">
                 <img 
@@ -39,6 +47,27 @@ const News = ({
                 <div dangerouslySetInnerHTML={{ __html: `${item.news.substring(0, 500)}...`}}></div>
               </div>
             </div>
+          )}
+
+          {title && news.length > 0 && news.map((item, idx) => 
+            item.title == title 
+            ?
+            <div 
+              key={idx} 
+              className="news-section-2-announcement"
+            >
+              <div className="news-section-2-announcement-title">{item.title} - {item.date}</div>
+              <div className="news-section-2-announcement-image">              
+                <img 
+                  src={`${PUBLIC_FILES}/news/${item.image}`} 
+                  alt={item.title}
+                  onError={(e) => e.target.src = 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled.png'}
+                />
+              </div>
+              <div className="news-section-2-announcement-paragraph" dangerouslySetInnerHTML={{ __html: `${item.news.substring(0, 500)}...`}}></div>
+            </div>
+            :
+            null
           )}
         </div>
       </div>
