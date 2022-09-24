994b3cc462b5f750ebb66e33072c036b102a93db
Fabricio <contact@fabricioguardia.com>
2022-09-23 19:01:14 -0700

    content security policy

--- pages/_app.js
+++ pages/_app.js
@@ -86,9 +86,10 @@ function MyApp({ Component, pageProps, client, account, accessToken, data, origi
   return <>
     <Head>
       <>
-      <title>CEAS</title>
+      <title>CATSUS</title>
       <meta charset="utf-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+      < meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
       <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet" />
       <script src="https://unpkg.com/@lottiefiles/lottie-player@0.4.0/dist/lottie-player.js"></script>
       </>
