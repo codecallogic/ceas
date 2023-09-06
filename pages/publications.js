e5e0392157afcb870137a5ff17efc98f7770493e
Fabricio <contact@fabricioguardia.com>
2023-09-05 22:58:34 -0500

    table of contents and filter

--- pages/publications.js
+++ pages/publications.js
@@ -13,10 +13,42 @@ const Publications = ({
   //// METHODS
   setOpenSearch
 }) => {
-  console.log(publications)
+  
+  const [publicationTypes, setPublicationTypes] = useState([])
+  const [filter, setFilter] = useState('type')
+
+  useEffect(() => {
+
+    const items = []
+        
+    if(publications.length > 0){
+
+      publications.map((item, idx) => {
+
+        if(!items.includes(item.type.trim())){
+          
+          items.push(item.type)
+          
+        } 
+      })
+
+      setPublicationTypes([...items])
+      
+    }
+    
+  }, [publications])
+
+  const scroolToElement = (id) => {
+    let element = document.getElementById(id.toLowerCase());
+    if(element){
+      element.scrollIntoView();
+    }
+  }
+  
   return (
    <>
    {/* <Toolbar></Toolbar> */}
+
    <Navigation 
       navMenus={navMenus} 
       openSearch={openSearch}
@@ -29,164 +61,220 @@ const Publications = ({
           The Center for Advancement toward Sustainable Urban Systems (CATSUS) is committed to disseminating its findings with the scientific community, academia, students and federal, state and local agencies, through various venues, including peer-reviewed journal publications, conference papers and presentations, books and technical reports. A comprehensive list of publications authored and co-authored by CATSUS faculty and students presenting results and findings of research developed at CATSUS are presented available in this page
         </p>
       </div>
-      <div className="publication-section-2 wrapper">
-        {validatePublicationTypes(publications, 'book') ? <div className="publication-section-2-title">Books</div> : '' }
-        <div className="publication-section-2-publications">
-          { publications.length > 0 && publications.map((item, idx) => 
-            item.type.toLowerCase() == 'book' ?
-            <div
-              key={idx} 
-              className="publication-section-2-publications-item"
-              onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
-            >
-              {item.authors.length > 0 && item.authors.map( (i) => 
-              <span>{i ? `${i},` : ''} </span>
-              )}
-
-              <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
-              <span>{item.title ? `"${item.title}"` : ''} </span>
-              {item.venues.length > 0 && item.venues.map( (i) => 
+      { filter == 'type' &&
+        <div className="publication-section-2 wrapper">
+          <div className="publication-section-2-contents">
+            {publicationTypes.length > 0 && publicationTypes.map((item, idx) =>  
+              publicationTypes.length == idx + 1 ? <span onClick={() => scroolToElement(item.trim())}>&nbsp; {item} </span> :  <span onClick={() => scroolToElement(item.trim())}>&nbsp; {item} | </span>
+            )}
+          </div>
+          <div className="publication-section-2-sort">
+            <span onClick={() => setFilter('type')}>Sort by Type |</span>
+            <span onClick={() => setFilter('year')}>&nbsp; Sort by Year</span>
+          </div>
+          {validatePublicationTypes(publications, 'book') ? <div className="publication-section-2-title">Books</div> : '' }
+          <div className="publication-section-2-publications">
+            { publications.length > 0 && publications.map((item, idx) => 
+              item.type.toLowerCase() == 'book' ?
+              <div
+                key={idx} 
+                id="book"
+                className="publication-section-2-publications-item"
+                onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
+              >
+                {item.authors.length > 0 && item.authors.map( (i) => 
                 <span>{i ? `${i},` : ''} </span>
-              )}
-              {/* <span>{item.type ? `${item.type}` : ''} </span> */}
-              
-            </div>
-            : 
-            null
-          )}
-        </div>
+                )}
 
-        {validatePublicationTypes(publications, 'book chapter') ? <div className="publication-section-2-title">Book Chapters</div> : '' }
-        <div className="publication-section-2-publications">
-          { publications.length > 0 && publications.map((item, idx) => 
-            item.type.toLowerCase() == 'book chapter' ?
-            <div
-              key={idx} 
-              className="publication-section-2-publications-item"
-              onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
-            >
-              {item.authors.length > 0 && item.authors.map( (i) => 
-              <span>{i ? `${i},` : ''} </span>
-              )}
+                <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
+                <span>{item.title ? `"${item.title}"` : ''} </span>
+                {item.venues.length > 0 && item.venues.map( (i) => 
+                  <span>{i ? `${i},` : ''} </span>
+                )}
+                {/* <span>{item.type ? `${item.type}` : ''} </span> */}
+                
+              </div>
+              : 
+              null
+            )}
+          </div>
 
-              <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
-              <span>{item.title ? `"${item.title}"` : ''} </span>
-              {item.venues.length > 0 && item.venues.map( (i) => 
+          {validatePublicationTypes(publications, 'book chapter') ? <div className="publication-section-2-title">Book Chapters</div> : '' }
+          <div className="publication-section-2-publications">
+            { publications.length > 0 && publications.map((item, idx) => 
+              item.type.toLowerCase() == 'book chapter' ?
+              <div
+                key={idx} 
+                id="book chapter"
+                className="publication-section-2-publications-item"
+                onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
+              >
+                {item.authors.length > 0 && item.authors.map( (i) => 
                 <span>{i ? `${i},` : ''} </span>
-              )}
-              {/* <span>{item.type ? `${item.type}` : ''} </span> */}
-              
-            </div>
-            : 
-            null
-          )}
-        </div>
+                )}
 
-        {validatePublicationTypes(publications, 'conference paper and presentation') ? <div className="publication-section-2-title">Conference Papers and Presentations</div> : '' }
-        <div className="publication-section-2-publications">
-          { publications.length > 0 && publications.map((item, idx) => 
-            item.type.toLowerCase() == 'conference paper and presentation' ?
-            <div
-              key={idx} 
-              className="publication-section-2-publications-item"
-              onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
-            >
-              {item.authors.length > 0 && item.authors.map( (i) => 
-              <span>{i ? `${i},` : ''} </span>
-              )}
+                <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
+                <span>{item.title ? `"${item.title}"` : ''} </span>
+                {item.venues.length > 0 && item.venues.map( (i) => 
+                  <span>{i ? `${i},` : ''} </span>
+                )}
+                {/* <span>{item.type ? `${item.type}` : ''} </span> */}
+                
+              </div>
+              : 
+              null
+            )}
+          </div>
 
-              <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
-              <span>{item.title ? `"${item.title}"` : ''} </span>
-              {item.venues.length > 0 && item.venues.map( (i) => 
+          {validatePublicationTypes(publications, 'conference paper and presentation') ? <div className="publication-section-2-title">Conference Papers and Presentations</div> : '' }
+          <div className="publication-section-2-publications">
+            { publications.length > 0 && publications.map((item, idx) => 
+              item.type.toLowerCase() == 'conference paper and presentation' ?
+              <div
+                key={idx} 
+                className="publication-section-2-publications-item"
+                id="conference paper and presentation"
+                onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
+              >
+                {item.authors.length > 0 && item.authors.map( (i) => 
                 <span>{i ? `${i},` : ''} </span>
-              )}
-              {/* <span>{item.type ? `${item.type}` : ''} </span> */}
-              
-            </div>
-            : 
-            null
-          )}
-        </div>
+                )}
 
-        {validatePublicationTypes(publications, 'journal and juried conference paper') ? <div className="publication-section-2-title">Journal and Juried Conference Paper</div> : '' }
-        <div className="publication-section-2-publications">
-          { publications.length > 0 && publications.map((item, idx) => 
-            item.type.toLowerCase() == 'journal and juried conference paper' ?
-            <div
-              key={idx} 
-              className="publication-section-2-publications-item"
-              onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
-            >
-              {item.authors.length > 0 && item.authors.map( (i) => 
-              <span>{i ? `${i},` : ''} </span>
-              )}
+                <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
+                <span>{item.title ? `"${item.title}"` : ''} </span>
+                {item.venues.length > 0 && item.venues.map( (i) => 
+                  <span>{i ? `${i},` : ''} </span>
+                )}
+                {/* <span>{item.type ? `${item.type}` : ''} </span> */}
+                
+              </div>
+              : 
+              null
+            )}
+          </div>
 
-              <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
-              <span>{item.title ? `"${item.title}"` : ''} </span>
-              {item.venues.length > 0 && item.venues.map( (i) => 
+          {validatePublicationTypes(publications, 'journal and juried conference paper') ? <div className="publication-section-2-title">Journal and Juried Conference Paper</div> : '' }
+          <div className="publication-section-2-publications">
+            { publications.length > 0 && publications.map((item, idx) => 
+              item.type.toLowerCase() == 'journal and juried conference paper' ?
+              <div
+                key={idx} 
+                className="publication-section-2-publications-item"
+                id="journal and juried conference paper"
+                onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
+              >
+                {item.authors.length > 0 && item.authors.map( (i) => 
                 <span>{i ? `${i},` : ''} </span>
-              )}
-              {/* <span>{item.type ? `${item.type}` : ''} </span> */}
-              
-            </div>
-            : 
-            null
-          )}
-        </div>
+                )}
 
-        {validatePublicationTypes(publications, 'theses and dissertations') ? <div className="publication-section-2-title">Theses and Dissertations</div> : '' }
-        <div className="publication-section-2-publications">
-          { publications.length > 0 && publications.map((item, idx) => 
-            item.type.toLowerCase() == 'theses and dissertations' ?
-            <div
-              key={idx} 
-              className="publication-section-2-publications-item"
-              onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
-            >
-              {item.authors.length > 0 && item.authors.map( (i) => 
-              <span>{i ? `${i},` : ''} </span>
-              )}
+                <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
+                <span>{item.title ? `"${item.title}"` : ''} </span>
+                {item.venues.length > 0 && item.venues.map( (i) => 
+                  <span>{i ? `${i},` : ''} </span>
+                )}
+                {/* <span>{item.type ? `${item.type}` : ''} </span> */}
+                
+              </div>
+              : 
+              null
+            )}
+          </div>
 
-              <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
-              <span>{item.title ? `"${item.title}"` : ''} </span>
-              {item.venues.length > 0 && item.venues.map( (i) => 
+          {validatePublicationTypes(publications, 'theses and dissertations') ? <div className="publication-section-2-title">Theses and Dissertations</div> : '' }
+          <div className="publication-section-2-publications">
+            { publications.length > 0 && publications.map((item, idx) => 
+              item.type.toLowerCase() == 'theses and dissertations' ?
+              <div
+                key={idx} 
+                className="publication-section-2-publications-item"
+                id="theses and dissertations"
+                onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
+              >
+                {item.authors.length > 0 && item.authors.map( (i) => 
                 <span>{i ? `${i},` : ''} </span>
-              )}
-              {/* <span>{item.type ? `${item.type}` : ''} </span> */}
-              
-            </div>
-            : 
-            null
-          )}
-        </div>
+                )}
 
-        {validatePublicationTypes(publications, 'other publications') ? <div className="publication-section-2-title">Other Publications</div> : '' }
-        <div className="publication-section-2-publications">
-          { publications.length > 0 && publications.map((item, idx) => 
-            item.type.toLowerCase() == 'other publications' ?
-            <div
-              key={idx} 
-              className="publication-section-2-publications-item"
-              onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
-            >
-              {item.authors.length > 0 && item.authors.map( (i) => 
-              <span>{i ? `${i},` : ''} </span>
-              )}
+                <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
+                <span>{item.title ? `"${item.title}"` : ''} </span>
+                {item.venues.length > 0 && item.venues.map( (i) => 
+                  <span>{i ? `${i},` : ''} </span>
+                )}
+                {/* <span>{item.type ? `${item.type}` : ''} </span> */}
+                
+              </div>
+              : 
+              null
+            )}
+          </div>
 
-              <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
-              <span>{item.title ? `"${item.title}"` : ''} </span>
-              {item.venues.length > 0 && item.venues.map( (i) => 
+          {validatePublicationTypes(publications, 'other publications') ? <div className="publication-section-2-title">Other Publications</div> : '' }
+          <div className="publication-section-2-publications">
+            { publications.length > 0 && publications.map((item, idx) => 
+              item.type.toLowerCase() == 'other publications' ?
+              <div
+                key={idx} 
+                id="other publications"
+                className="publication-section-2-publications-item"
+                onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
+              >
+                {item.authors.length > 0 && item.authors.map( (i) => 
                 <span>{i ? `${i},` : ''} </span>
-              )}
-              {/* <span>{item.type ? `${item.type}` : ''} </span> */}
-              
-            </div>
-            : 
-            null
-          )}
-        </div>
-        
+                )}
+
+                <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
+                <span>{item.title ? `"${item.title}"` : ''} </span>
+                {item.venues.length > 0 && item.venues.map( (i) => 
+                  <span>{i ? `${i},` : ''} </span>
+                )}
+                {/* <span>{item.type ? `${item.type}` : ''} </span> */}
+                
+              </div>
+              : 
+              null
+            )}
+          </div>
       </div>
+      }
+
+      { filter == 'year' &&
+        <div className="publication-section-2 wrapper">
+          <div className="publication-section-2-contents">
+            {publicationTypes.length > 0 && publicationTypes.map((item, idx) =>  
+              publicationTypes.length == idx + 1 ? <span onClick={() => scroolToElement(item.trim())}>&nbsp; {item} </span> :  <span onClick={() => scroolToElement(item.trim())}>&nbsp; {item} | </span>
+            )}
+          </div>
+          <div className="publication-section-2-sort">
+            <span onClick={() => setFilter('type')}>Sort by Type |</span>
+            <span onClick={() => setFilter('year')}>&nbsp; Sort by Year</span>
+          </div>
+          <br />
+          <br />
+          <div className="publication-section-2-title">Publications by Year</div>
+          <div className="publication-section-2-publications">
+            { publications.length > 0 && publications.sort((a, b) => new Date(b.date) - new Date(a.date)).map((item, idx) => 
+              <div
+                key={idx} 
+                id="book"
+                className="publication-section-2-publications-item"
+                onClick={(e) => window.open(`${PUBLIC_FILES}/publication/${item.file}`, '_blank')}
+              >
+                {item.authors.length > 0 && item.authors.map( (i) => 
+                <span>{i ? `${i},` : ''} </span>
+                )}
+
+                <span>{item.date ? `(${item.date.split('/')[2]}).` : ''} </span>
+                <span>{item.title ? `"${item.title}"` : ''} </span>
+                {item.venues.length > 0 && item.venues.map( (i) => 
+                  <span>{i ? `${i},` : ''} </span>
+                )}
+                
+              </div>
+            )}
+          </div>
+
+      </div>
+      }
+      
     </div>
     <Footer></Footer>
    </>
