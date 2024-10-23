adc48343eaa89e3ed29ee3449364fd99465e2600
Fabricio <contact@fabricioguardia.com>
2024-10-22 18:33:38 -0700

    pm2 config update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,8 +13,7 @@ module.exports = {
       repo : 'git@github.com:codecallogic/ceas.git',
       path : '/home/ubuntu/client',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install --legacy-peer-deps && NODE_OPTIONS="--openssl-legacy-provider" && npm run build && pm2 reload ecosystem.config.js --env production',
-
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install --legacy-peer-deps && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
